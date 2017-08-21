5f1e6107556b212cd8f669f0288528f9e569fb1c
Siddhan <SiddhanS@dnb.com>
2017-08-21 16:11:18 +0100

    Spring Security Build

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -4,17 +4,10 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestOperations;
-import org.springframework.web.client.RestTemplate;
 
 
 @Configuration
 public class RestConfig {
-
-    @Bean
-    public RestOperations restOperations() {
-        return new RestTemplate();
-    }
 
     @Bean
     public ObjectMapper objectMapper() {
